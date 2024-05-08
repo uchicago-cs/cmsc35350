5db3a75b26d27b4bdbb66846ee46c016df8118ab
Ian Foster <foster@anl.gov>
2024-05-08 13:34:50 -0500

    Update llm_check_relevance.py

--- source/llm_check_relevance.py
+++ source/llm_check_relevance.py
@@ -79,19 +79,19 @@ def extract_scores(paperId, message):
 in_path  = f'{dataset}/{dataset}.jsonl'
 out_path = f'{dataset}/{dataset}_scores.csv'
 
-# Find what we already have
-with open(out_path, 'r') as in_file:
-    csv_reader = csv.reader(in_file)
-    ids_already_read = []
-    for row in csv_reader:
-        if row:  # Check if row is not empty
-            ids_already_read.append(row[0])
+if os.path.isfile(out_path):
+    with open(out_path, 'r') as in_file:
+        csv_reader = csv.reader(in_file)
+        ids_already_read = []
+        for row in csv_reader:
+            if row:  # Check if row is not empty
+                ids_already_read.append(row[0])
 
 print('Already read:', ids_already_read)
 
 count = 0
 
-with open(out_path, 'a') as out_file:
+with open(out_path, 'a+') as out_file:
     writer = csv.writer(out_file)
     with open(in_path, 'r' ) as in_file:
         for line in in_file:
