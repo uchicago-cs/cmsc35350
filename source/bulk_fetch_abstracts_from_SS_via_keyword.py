dd508887ad5ab3570c1293fb5cb62a3274b512d1
Ian Foster <foster@anl.gov>
2024-05-08 06:19:41 -0500

    Update bulk_fetch_abstracts_from_SS_via_keyword.py

--- source/bulk_fetch_abstracts_from_SS_via_keyword.py
+++ source/bulk_fetch_abstracts_from_SS_via_keyword.py
@@ -1,57 +1,85 @@
+#!/usr/bin/env python3
+
 import requests
 import random
 import json
 import sys
 import time
 from pathlib import Path
+import argparse
 
-if len(sys.argv) != 4:
-    print("Usage: source/python3.11 bulk_fetch_via_keyword.py 'search_term' year filename")
-    sys.exit(1)
+max_attempts = 3
+max_pause    = 5
 
-query   = sys.argv[1]
-year    = sys.argv[2]
-dataset = sys.argv[3]
+def attempt_to_retrieve(url):
+    for attempt in range(max_attempts):
+        try:
+            print('Attempt', url)
+            r = requests.get(url).json()
+            print(f"  Will retrieve an estimated {r['total']} documents")
+            return r
+        except Exception as e:
+            print(f"Attempt {attempt+1} failed: {e}")
+            if attempt < max_attempts - 1:
+                pause_time = random.uniform(0, max_pause) + 1
+                print(f"Pausing for {pause_time:.2f} seconds...")
+                time.sleep(pause_time)
+            else:
+                print("All attempts failed.")
+                return Null
 
-print(f'Query = {query}; year = {year}')
 
-fields = 'abstract,title,year'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', '-d', type=str, required=True)
+    parser.add_argument('--file',    '-f', type=str)
+    parser.add_argument('--query',   '-q', type=str)
+    parser.add_argument('--year',    '-y', type=str)
+    args = parser.parse_args()
 
-url = f'http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}&year={year}'
+    dataset = args.dataset
 
-max_attempts = 3                                                                                                                        
-max_pause = 5                                                                                                                           
-for attempt in range(max_attempts):                                                                                                     
-    try:
-        r = requests.get(url).json()
-        print(f"  Will retrieve an estimated {r['total']} documents")
-        break                                                                                                                        
-    except Exception as e:
-        print(f"Attempt {attempt+1} failed: {e}")
-        if attempt < max_attempts - 1:
-            pause_time = random.uniform(0, max_pause) + 1
-            print(f"Pausing for {pause_time:.2f} seconds...")
-            time.sleep(pause_time)
-        else:
-            print("All attempts failed.")
-            exit(1)
+    if args.year == None:
+        years = range(2016,2025)
+    else:
+        years = [args.year]
 
-retrieved = 0
+    if args.query == None and args.file == None:
+        print('Need query or query file')
+        exit(1)
+    if args.query != None and args.file != None:
+        print('Cannot have both query and query file')
+        exit(1)
 
-directory_path = Path(f'{dataset}')
-if not directory_path.exists():
-    directory_path.mkdir(parents=True, exist_ok=True)
+    if args.query != None:
+        queries = [args.query]
+    else:
+        # Open file containing a list of search terms
+        with open(args.file, 'r') as file:
+            queries = file.readlines()
+            queries = [q.strip() for q in queries]  #
 
-with open(f'{dataset}/{dataset}.jsonl', 'a') as file:
-    while True:
-        if 'data' in r:
-            retrieved += len(r['data'])
-            for paper in r['data']:
-                print(json.dumps(paper), file=file)
-        if 'token' not in r:
-            break
-        r = requests.get(f"{url}&token={r['token']}").json()
+    fields = 'abstract,title,year'
 
-print(f'  Retrieved {retrieved} papers total')
+    # Create <dataset> directory if it does not already exist
+    directory_path = Path(f'{dataset}')
+    if not directory_path.exists():
+        directory_path.mkdir(parents=True, exist_ok=True)
 
-time.sleep(1)
+    retrieved = 0
+    with open(f'{dataset}/{dataset}.jsonl', 'a+') as file:
+        for query in queries:
+            for year in years:
+                url = f'http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}&year={year}'
+                while True:
+                    r = attempt_to_retrieve(url)
+                    print(r)
+                    if 'data' in r:
+                        retrieved += len(r['data'])
+                        for paper in r['data']:
+                            print(json.dumps(paper), file=file)
+                    if 'token' not in r:
+                        break
+                    r = requests.get(f"{url}&token={r['token']}").json()
+                    time.sleep(1)
+
