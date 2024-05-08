804dcc0a617c8567459ce25d84d129b6123fe496
Ian Foster <foster@anl.gov>
2024-05-07 20:20:37 -0500

    Update llm_summarize_all_documents.py

--- source/llm_summarize_all_documents.py
+++ source/llm_summarize_all_documents.py
@@ -7,18 +7,18 @@ import openai
 port = 80
 
 if len(sys.argv) != 4:
-    print('Usage: python3.11 sourcce/llm_summarize_all_documents.py <ip_address> <api_key> <dataset>')
+    print('Usage: python3.11 sourcce/llm_summarize_all_documents.py <dataset> <ip_address> <api_key>')
     exit(1)
 
-ip_address = sys.argv[1]
-api_key    = sys,argv[2]
-dataset    = sys.argv[3]
+dataset    = sys.argv[1]
+ip_address = sys.argv[2]
+api_key    = sys,argv[3]
 
 
 # Set OpenAI's API key and API base to use vLLM's API server.
 openai_api_key = api_key
 openai_api_base = f"http://{ip_address}:{port}/v1"
-print('Connecting to', openai_api_base)
+
 
 def create_prompt(chunk):
     gpt_user_prompt = "Please summarize in approximately 800 words this paper. [## BEGIN PAPER " + chunk + " END PAPER  ##] Your summary should contain the TITLE of the paper, the YEAR the paper was published, the KEY FINDINGS, the MAIN RESULT, one novel HYPOTHESIS the paper proposes or that you can infer from the text when the hypothesis is not explicit. Please propose an EXPERIMENT that would validate the hypothesis; be specific about required equipment and steps to follow. In addition to generating the summary, please generate a list of up to ten KEYWORDS that are relevant to the paper.  Please output your response as a valid JSON document with the UPPER CASE words as keys, and no other text before or after the JSON document."
